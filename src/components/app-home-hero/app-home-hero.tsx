2d719404a3103116d5d1ee6f891bddb27d99acf7
Alex Arriaga <alexzander.arriaga@base22.com>
2019-04-09 09:19:26 -0500

    Mejorar estilos visuales del sitio

--- src/components/app-home-hero/app-home-hero.tsx
+++ src/components/app-home-hero/app-home-hero.tsx
@@ -5,14 +5,14 @@ export const HomeHero = () => {
       <div class="b-hero">
         <div class="b-hero__image b-image-container p-5 text-center">
           <img
-            src="http://conference.webtraining.fun/img/welcome/connection.png"
+            src="/assets/bg-home.png"
             alt="Let's go!"
             class="img-fluid"/>
         </div>
 
         <div class="b-hero__text pt-5 pb-5">
             <span class="b-hero__text-top">
-                01 y 02 de noviembre de 2018
+                01 y 02 de Mayo de 2019
             </span>
           <span class="b-hero__text-bottom">
                 Monterrey, NL, México
--- src/components/app-program/app-program.tsx
+++ src/components/app-program/app-program.tsx
@@ -1,15 +1,17 @@
 import {Component} from '@stencil/core';
+// import {RouterHistory} from '@stencil/router';
 
 @Component({
   tag: 'app-program',
 })
 export class AppProgram {
+  // @Prop() history?: RouterHistory;
 
   render() {
 
     return (
       <div>
-        <app-program-agenda/>
+        <app-program-agenda />
       </div>
     );
   }
